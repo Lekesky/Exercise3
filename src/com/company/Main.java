ab8feda1f0c6e72a183c03199e6d30aaba588424
Lekesky <61468937+Lekesky@users.noreply.github.com>
2022-02-02 13:56:11 -0500

    restart

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,15 +7,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a temperature in Fahrenheit");
-        int degree = input.nextInt();
-        convertTemp(degree);
 
 
-//        System.exit(0);
     }
 
     static void convertTemp(double temp){
         var celsius = (temp-32) * 5/9;
-        return;
+        System.out.println(celsius + "C");
     }
 }
+
