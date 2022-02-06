cfb50cfdbf7b83dcc6b176f4cbbe8777884744d3
Lekesky <61468937+Lekesky@users.noreply.github.com>
2022-02-06 02:00:10 -0500

    Final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,20 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a temperature in Fahrenheit");
-
-
-    }
+    static Scanner input = new Scanner(System.in);
 
     static void convertTemp(double temp){
         var celsius = (temp-32) * 5/9;
         System.out.println(celsius + "C");
+    }
+    public static void main(String[] args) {
+        int fahre;
+        do{
+            System.out.println("Enter a temperature in Fahrenheit");
+            int fahre1 = input.nextInt();
+            fahre = fahre1;
+            convertTemp(fahre);
+        }while(fahre >= -460);
     }
 }
 
