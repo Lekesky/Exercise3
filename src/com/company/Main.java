870004611e8a557d9f0cfe8a2794e1f82cc86cf8
Lekesky <61468937+Lekesky@users.noreply.github.com>
2022-02-06 02:05:04 -0500

    Final w/ Comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,7 @@
+// Leke Obayemi
+// 2/6/22
+// exerecise 3
+
 package com.company;
 
 import java.util.Scanner;
@@ -5,13 +9,13 @@ import java.util.Scanner;
 public class Main {
     static Scanner input = new Scanner(System.in);
 
-    static void convertTemp(double temp){
+    static void convertTemp(double temp){   //Converts Fahrenheit to Celsius
         var celsius = (temp-32) * 5/9;
         System.out.println(celsius + "C");
     }
     public static void main(String[] args) {
         int fahre;
-        do{
+        do{         //Prompts user to enter temp always till the user enters a temp less than -460
             System.out.println("Enter a temperature in Fahrenheit");
             int fahre1 = input.nextInt();
             fahre = fahre1;
